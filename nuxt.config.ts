f9917549b88096dc87a66e09e6f1e9a0c427a872
Tnixc <tnixxc@gmail.com>
2024-02-10 17:11:02 -0500

    Refactor code for improved performance

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxtjs/i18n",
+    "@nuxtjs/color-mode",
+    "@nuxt/image",
+    "vue3-carousel-nuxt",
+  ],
+});
