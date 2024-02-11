4350c644ea476f91b9341c49ac67cf94a9fd6ea5
Tnixc <tnixxc@gmail.com>
2024-02-10 20:04:57 -0500

    Add dropdown menu and button to Nav.vue

--- components/Global/Nav.vue
+++ components/Global/Nav.vue
@@ -10,6 +10,18 @@
           <option value="dark">Dark</option>
         </select>
       </div>
+      <Button>Click me</Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   </nav>
 </template>
