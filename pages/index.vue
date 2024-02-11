b6c365f6c553be68637339e0f38f92f69e9e14c2
Tnixc <tnixxc@gmail.com>
2024-02-11 13:28:54 -0500

    Add language support and download and about links to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
+<script setup>
+const { locale } = useI18n();
+</script>
 <template>
   <Nav />
   <Container>
@@ -5,6 +8,25 @@
     <IndexWhyUs />
     <IndexFeatures />
     <IndexAdvantage />
+    <div class="flex gap-4 p-10">
+      <NuxtLink
+        class="flex h-fit items-center gap-2 border bg-blue-600 p-5 font-mono text-2xl text-white hover:underline"
+        :to="`/${locale}/download`"
+      >
+        <Icon
+          name="material-symbols:download-sharp"
+          class="aspect-square text-3xl text-white"
+        />
+        <span class="">{{ $t("common.download") }}</span>
+      </NuxtLink>
+      <NuxtLink
+        class="flex h-fit items-center gap-2 border bg-accent p-5 px-10 font-mono text-2xl hover:underline"
+        :to="`/${locale}/about`"
+      >
+        {{ $t("common.about") }}
+        <Icon name="ph:arrow-right-bold" />
+      </NuxtLink>
+    </div>
   </Container>
   <Footer />
 </template>
