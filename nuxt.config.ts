cfa1032865d7ccf8d49e06dd8b13f16abdc5f23b
Tnixc <tnixxc@gmail.com>
2024-02-11 16:46:50 -0500

    Remove unused app configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
   css: ["@/assets/global.css"],
   modules: [
     "@nuxtjs/tailwindcss",
